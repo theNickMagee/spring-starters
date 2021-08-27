63e6c1e71ee24dc1736351f54804ec1e080108fb
Digital publishing <digitalpublishinginc@iMac-4.local>
2021-08-27 16:50:17 -0500

    initial commit

---
+++ course-api-data/src/main/java/io/javabrains/springbootquickstart/courses/CourseRepository.java
@@ -0,0 +1,8 @@
+package io.javabrains.springbootquickstart.courses;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseRepository extends CrudRepository<Course, String> { 
+	
+	
+}
---
+++ course-api/src/main/java/io/javabrains/springbootquickstart/hello/HelloController.java
@@ -0,0 +1,17 @@
+package io.javabrains.springbootquickstart.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/hello")
+	public String sayHi() {
+		return "Hi";
+	}
+	@RequestMapping("/bye")
+	public String sayBye() {
+		return "Bye";
+	}
+}
